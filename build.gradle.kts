74cf5e93adeea29004db176f93814feb2d2aa943
David <david.schlachter@mindmatrix.net>
2022-03-14 23:19:29 -0700

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+    java
+}
+
+group = "com.rshub"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.github.RSKraken:KrakenAPI:master-SNAPSHOT")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
