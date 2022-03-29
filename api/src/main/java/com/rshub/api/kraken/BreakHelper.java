404e12644c935225fcf7d9d10806a85df41a5995
DrJavatar <david.schlachter@mindmatrix.net>
2022-03-29 00:36:02 -0700

    added random break methods.

--- api/src/main/java/com/rshub/api/kraken/BreakHelper.java
+++ api/src/main/java/com/rshub/api/kraken/BreakHelper.java
@@ -1,6 +1,7 @@
 package com.rshub.api.kraken;
 
 import kraken.plugin.api.Kraken;
+import kraken.plugin.api.Rng;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,5 +15,13 @@ public final class BreakHelper {
 
     public static void takeBreak(long time, TimeUnit unit) {
         Kraken.takeBreak(unit.toMillis(time));
+    }
+
+    public static void takeRandomBreak(long min, long max, TimeUnit tmin, TimeUnit mmin) {
+        Kraken.takeBreak(Rng.i32((int) tmin.toMillis(min), (int) mmin.toMillis(max)));
+    }
+
+    public static void takeRandomBreak(long min, long max, TimeUnit unit) {
+        takeBreak(Rng.i32((int) min, (int) max), unit);
     }
 }
