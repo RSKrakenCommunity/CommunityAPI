6706b035e4665a5b3a13b68ec25aabc1d8d5ae15
DrJavatar <david.schlachter@mindmatrix.net>
2022-04-11 18:48:02 -0700

    Added bank locations.

---
+++ api/src/main/kotlin/com/rshub/api/world/BankLocations.kt
@@ -0,0 +1,60 @@
+package com.rshub.api.world
+
+import com.rshub.api.banking.BankLocation
+import com.rshub.definitions.maps.WorldTile
+
+public object BankLocations {
+  public val EDGE_BANK: BankLocation = BankLocation("Edge Bank", WorldTile(3093, 3494, 0))
+
+  public val CLAN_BANK: BankLocation = BankLocation("Clan Bank", WorldTile(2957, 3296, 0))
+
+  public val TAVERLY_BANK: BankLocation = BankLocation("Taverly Bank", WorldTile(2877, 3417, 0))
+
+  public val LUMBRIDGE_BANK: BankLocation = BankLocation("Lumbridge Bank", WorldTile(3212, 3257, 0))
+
+  public val DUNG: BankLocation = BankLocation("Dung", WorldTile(3450, 3730, 0))
+
+  public val `CHAOS_ALTER_(WILDY)`: BankLocation = BankLocation("Chaos Alter (Wildy)",
+      WorldTile(3236, 3602, 0))
+
+  public val KHARID_WEST_BANK: BankLocation = BankLocation("Kharid West Bank", WorldTile(3271, 3166,
+      0))
+
+  public val DUAL_AREA_BANK: BankLocation = BankLocation("Dual Area Bank", WorldTile(3381, 3270, 0))
+
+  public val DRAYNOR_VILLAGE_BANK: BankLocation = BankLocation("Draynor Village Bank",
+      WorldTile(3094, 3243, 0))
+
+  public val FALADOR_WEST_BANK: BankLocation = BankLocation("Falador West Bank", WorldTile(2946,
+      3370, 0))
+
+  public val FALADOR_EAST_BANK: BankLocation = BankLocation("Falador East Bank", WorldTile(3013,
+      3356, 0))
+
+  public val FALADOR_WORKSHOP_BANK: BankLocation = BankLocation("Falador Workshop Bank",
+      WorldTile(3059, 3339, 0))
+
+  public val BURTHORPE_BANK: BankLocation = BankLocation("Burthorpe Bank", WorldTile(2891, 3538, 0))
+
+  public val CATHERBY_BANK: BankLocation = BankLocation("Catherby Bank", WorldTile(2796, 3439, 0))
+
+  public val SEERS_BANK: BankLocation = BankLocation("Seers Bank", WorldTile(2725, 3491, 0))
+
+  public val POF_BANK_CHEST: BankLocation = BankLocation("POF Bank Chest", WorldTile(2651, 3348, 0))
+
+  public val ARDOUGNE_NORTH_BANK: BankLocation = BankLocation("Ardougne North Bank", WorldTile(2617,
+      3333, 0))
+
+  public val ARDOUGNE_SOUTH_BANK: BankLocation = BankLocation("Ardougne South Bank", WorldTile(2652,
+      3284, 0))
+
+  public val YANILLE_BANK: BankLocation = BankLocation("Yanille Bank", WorldTile(2611, 3092, 0))
+
+  public val CASTLE_WARS_BANK_CHEST: BankLocation = BankLocation("Castle Wars Bank Chest",
+      WorldTile(2444, 3086, 0))
+
+  public val `OO'GLOG_BANK`: BankLocation = BankLocation("Oo'glog Bank", WorldTile(2557, 2839, 0))
+
+  public val VARROCK_NORTH_BANK: BankLocation = BankLocation("Varrock North Bank", WorldTile(3257,
+      3503, 0))
+}
