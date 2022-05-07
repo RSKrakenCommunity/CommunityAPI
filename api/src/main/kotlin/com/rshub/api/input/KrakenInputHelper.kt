7dca06ff1ebe3fc23bb0cf26a5709d9df8fc8da5
DrJavatar <david.schlachter@mindmatrix.net>
2022-05-06 18:50:57 -0700

    Updated Community api input to allow press and release.

--- api/src/main/kotlin/com/rshub/api/input/KrakenInputHelper.kt
+++ api/src/main/kotlin/com/rshub/api/input/KrakenInputHelper.kt
@@ -1,8 +1,37 @@
 package com.rshub.api.input
 
+import kotlinx.coroutines.delay
 import kraken.plugin.api.Input
+import java.util.function.Consumer
 
 object KrakenInputHelper {
+
+    suspend fun typeKey(key: Int, delay: Long) {
+        pressKey(key)
+        delay(delay)
+        releaseKey(key)
+    }
+
+    suspend fun typeKey(literal: Char, delay: Long) {
+        typeKey(literal.code, delay)
+    }
+
+    @JvmStatic
+    fun doKey(code: Int, consumer: Consumer<Void?>) {
+        pressKey(code)
+        consumer.accept(null)
+        releaseKey(code)
+    }
+
+    @JvmStatic
+    fun pressKey(code: Int) {
+        Input.press(code)
+    }
+
+    @JvmStatic
+    fun releaseKey(code: Int) {
+        Input.release(code)
+    }
 
     @JvmStatic
     fun typeKey(code: Int) {
